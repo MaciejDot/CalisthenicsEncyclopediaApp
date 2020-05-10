771fdfa55e330e8d7692bb8a8be137f5218c866b
Maciek_dot <maciejd0@op.pl>
2020-05-10 18:38:34 +0200

    slowly modifing to newer version

--- Frontend/app/src/App.vue
+++ Frontend/app/src/App.vue
@@ -1,32 +1,54 @@
 <template>
   <div id="app">
-    <div id="nav">
-      <router-link to="/">Home</router-link> |
-      <router-link to="/about">About</router-link>
-    </div>
+    <b-navbar
+      id="navbar"
+      toggleable="lg"
+      type="light"
+      variant="white"
+      :sticky="true"
+    >
+      <b-navbar-brand to="/">Calisthenics Encyclopedia</b-navbar-brand>
+      <b-navbar-toggle
+        id="navbar-toggle"
+        target="nav-collapse"
+      ></b-navbar-toggle>
+      <b-collapse id="nav-collapse" is-nav>
+        <b-navbar-nav>
+          <b-nav-item>
+            <router-link class="nav-link" to="/about">
+              Workout Program
+            </router-link>
+          </b-nav-item>
+        </b-navbar-nav>
+      </b-collapse>
+    </b-navbar>
     <router-view />
   </div>
 </template>
-
+<script>
+import { BNavbar } from "bootstrap-vue";
+export default {
+  name: "App",
+  components: {
+    BNavbar
+  }
+};
+</script>
 <style lang="scss">
 #app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
+  font-family: Georgia, serif;
   -webkit-font-smoothing: antialiased;
   -moz-osx-font-smoothing: grayscale;
   text-align: center;
   color: #2c3e50;
 }
-
-#nav {
-  padding: 30px;
-
-  a {
-    font-weight: bold;
-    color: #2c3e50;
-
-    &.router-link-exact-active {
-      color: #42b983;
-    }
-  }
+#navbar {
+  border: 1px solid #e7e7e7;
+}
+#navbar-toggle {
+  border-radius: 1px;
+}
+h1 {
+  margin: 20px !important;
 }
 </style>
--- Frontend/app/src/views/Home.vue
+++ Frontend/app/src/views/Home.vue
@@ -1,18 +1,21 @@
 <template>
   <div class="home">
-    <img alt="Vue logo" src="../assets/logo.png" />
-    <HelloWorld msg="Welcome to Your Vue.js App" />
+    <hello-world msg="Welcome to Your Vue.js App" />
+    <b-icon-arrow-up></b-icon-arrow-up>
+    <b-card />
   </div>
 </template>
 
 <script>
 // @ is an alias to /src
 import HelloWorld from "@/components/HelloWorld.vue";
-
+import { BCard, BIconArrowUp } from "bootstrap-vue";
 export default {
   name: "Home",
   components: {
-    HelloWorld
+    BCard,
+    HelloWorld,
+    BIconArrowUp
   }
 };
 </script>
